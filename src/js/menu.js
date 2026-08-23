a64d60512442d9b440645fea2d0340725e9be17f
Alex Urquiza <alex.urz20@gmail.com>
2026-08-23 01:49:48 -0300

    El menu se desplaza y contrae al clickear

---
+++ src/js/menu.js
@@ -0,0 +1,18 @@
+const header = document.querySelector('header');
+
+const alternarMenu = () => {
+    const contenedor = document.querySelector('.menu-principal');
+    contenedor.classList.toggle('active-Nav');
+}
+
+header.addEventListener('click', (event) => {
+    // Si no clickea un boton, anula.
+    if(!event.target.closest('button')) return
+
+    if(event.target.closest('#btn-Menu')){
+        alternarMenu();
+    }
+});
+
+// hacer en un js aparte que el sidebar se comprima y expanda para luego exportar su funcion en este .js
+// y usarlo cuando se activa el menu.
