c250e630a76327170969962cc3ffcbb6c327badd
Alex Urquiza <alex.urz20@gmail.com>
2026-09-09 01:55:27 -0300

    Deuda tecnica (52): Error de responsive en importe

--- src/js/crearTransaccion/crearTransaccion.js
+++ src/js/crearTransaccion/crearTransaccion.js
@@ -23,13 +23,13 @@ export function crearTransaccion(items, hoy){
     li.className = "isTransaccion";
     li.innerHTML = `
         <span class="text-Listado ttl-transaccion">${name}</span>
-        <div class="cat-transaccion"">
+        <div class="cat-transaccion">
             <span class="material-symbols-outlined">${icono}</span>
             <span class="text-Listado">${categoria}</span>
         </div>
         <div class="extras-transaccion">
             <span class="text-Listado importe">$${importe}</span>
-            <span class="text-Listado">${horario}</span>
+            <span class="text-Listado isHorario">${horario}</span>
         </div>
     `;
 
