7fe6704d45c3d74d2308f57a9a0b2400d019849d
Alex Urquiza <alex.urz20@gmail.com>
2026-09-10 23:26:39 -0300

    Deuda tecnica (59): Optimizando menu.js

--- src/js/menu/menu.js
+++ src/js/menu/menu.js
@@ -5,28 +5,39 @@ import {
 } from "../sidebar/barrel_sidebar.js";
 
 const header = document.querySelector('header');
+const contenedor = document.querySelector('.menu-principal');
 
-const alternarMenu = () => {
-    const contenedor = document.querySelector('.menu-principal');
-    const main_sidebar = document.querySelector('aside.main-sidebar');
-    let estadoSidebar = recuperarEstado();
+const estadoMenu = {
+    estado: false                           // true: abierto || false: cerrado.
+}
 
-    if(contenedor.classList.contains('active-Nav')){
-        contenedor.classList.remove('active-Nav');
-        if(estadoSidebar){
-            expandirSidebar(main_sidebar);
-        }
+const abrirMenu = (main_sidebar) => {
+    contenedor.classList.add('active-Nav');
+    comprimirSidebar(main_sidebar);
+    estadoMenu.estado = true;
+}
+
+const cerrarMenu = (main_sidebar) => {
+    const estadoSidebar = recuperarEstado();
+    contenedor.classList.remove('active-Nav');
+    if(estadoSidebar){
+        expandirSidebar(main_sidebar);
+    }
+    estadoMenu.estado = false;
+}
+
+const alternarMenu = (main_sidebar) => {
+    if(estadoMenu.estado){
+        cerrarMenu(main_sidebar);
     } else {
-        contenedor.classList.add('active-Nav');
-        comprimirSidebar(main_sidebar);
+        abrirMenu(main_sidebar);
     }
 }
 
 header.addEventListener('click', (event) => {
-    if(!event.target.closest('button')) return
+    const btn_menu = event.target.closest('#btn-Menu');
+    if(!btn_menu) return;
 
-    if(event.target.closest('#btn-Menu')){
-        alternarMenu();
-
-    }
+    const main_sidebar = document.querySelector('aside.main-sidebar');
+    alternarMenu(main_sidebar);
 });
